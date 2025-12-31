46848bf8b7193f17ad011901269f096150903aa6
Sakshi Mude <sakshimude22@gmail.com>
2025-12-31 19:16:38 +0530

    Check if the number is Prime

---
+++ TakeUForward/Basicmaths/checkprime.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPrime(int n) {
+        if (n <= 1) return false;   
+        if (n == 2) return true;    
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                return false;       
+            }
+        }
+        return true;  
+    }
+};
