2ec53cfbb882bb039af56a53bd942d4b09be8c63
Sakshi Mude <sakshimude22@gmail.com>
2026-01-09 21:11:12 +0530

    Pattern 7

---
+++ TakeUForward/Patterns/pattern7.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void pattern7(int n){
+    for(int i = 1;i<=n;i++){
+        for(int j=1;j<=n-i;j++){
+            cout<<" ";
+        }
+        for(int j =1;j<=2*i-1;j++){
+            cout<<"*";
+        }
+        for(int j=1;j<=n-i;j++){
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+}
+int main(){
+    int n;
+    cin>>n;
+    pattern7(n);
+}
