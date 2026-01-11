87cfc8cd4279a48da09cc4f5b10019f04f84a201
Sakshi Mude <sakshimude22@gmail.com>
2026-01-11 21:59:49 +0530

    Pattern 8

---
+++ TakeUForward/Patterns/pattern8.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void pattern8(int n){
+    for(int i = 1;i<=n;i++){
+         for(int j=1;j<=i;j++){
+            cout<<" ";
+        }   
+        for(int j =1;j<=2*n-(2*i-1);j++){
+            cout<<"*";
+        }
+        
+       
+        cout<<endl;
+    }
+}
+int main(){
+    int n;
+    cin>>n;
+    pattern8(n);
+}
