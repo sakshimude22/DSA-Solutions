18080c4a752482c4f925346f77cd5a8a46910c5e
Sakshi Mude <sakshimude22@gmail.com>
2026-01-13 20:19:51 +0530

    Pattern 13

---
+++ TakeUForward/Patterns/pattern13.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void pattern13(int n){
+    int start =1;
+  for(int i =1;i<=n;i++){
+    
+     
+    for(int j=1;j<=i;j++){
+        cout<<start<<" ";
+       start=start+1;
+    }
+    cout<<endl;
+  }
+}
+int main(){
+    int n;
+    cin>>n;
+    pattern13(n);
+}
