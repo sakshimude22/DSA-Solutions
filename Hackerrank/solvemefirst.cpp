630df328402ea23b83f72dee226604497337bb97
Sakshi Mude <sakshimude22@gmail.com>
2026-01-23 23:26:38 +0530

    Add HackerRank solution: Solve Me First

---
+++ Hackerrank/solvemefirst.cpp
@@ -0,0 +1,10 @@
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    int a;
+    int b;
+    int sum;
+    cin >> a >> b;
+    sum = a+b;
+    cout << sum;
+    return 0;
+}
