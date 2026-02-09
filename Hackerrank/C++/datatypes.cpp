1f9f224334445b38dcb5ff13f2f6db7517c2698b
Sakshi Mude <sakshimude22@gmail.com>
2026-02-09 23:28:53 +0530

    Basic Data Types

---
+++ Hackerrank/C++/datatypes.cpp
@@ -0,0 +1,18 @@
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;
+    
+    cin >> a  >> b  >> c  >> d >> e;
+    cout << a << endl ;
+    cout << b << endl;
+    cout << c << endl;
+    cout << fixed << setprecision(3) << d << endl ;
+    cout << fixed << setprecision(9) << e << endl;
+    
+    return 0;
+    
+}
