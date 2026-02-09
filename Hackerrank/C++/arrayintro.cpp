bd2463274f4e43b9d710c03cba47a38e595481c9
Sakshi Mude <sakshimude22@gmail.com>
2026-02-09 23:27:50 +0530

    Arrays Intro

---
+++ Hackerrank/C++/arrayintro.cpp
@@ -0,0 +1,17 @@
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    int n;
+    cin >> n;
+    
+    vector<int> arr(n);
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    
+    reverse(arr.begin(), arr.end());
+    
+    for(int i = 0; i < n;i++){
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
