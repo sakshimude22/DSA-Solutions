1f108111bdd7e943f742967c208150077d66c90c
Sakshi Mude <sakshimude22@gmail.com>
2026-02-11 21:07:44 +0530

    Array Introduction

--- Hackerrank/C++/arrayintro.cpp
+++ Hackerrank/C++/arrayintro.cpp
@@ -1,3 +1,11 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
     int n;
