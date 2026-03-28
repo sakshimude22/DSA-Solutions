53fcbcdb5bbef6ddb734d4960f222425d8791bb4
Sakshi Mude <sakshimude22@gmail.com>
2026-03-28 21:41:44 +0530

    Top earners

---
+++ Hackerrank/SQL/topearners.sql
@@ -0,0 +1,9 @@
+-- using ms sql server
+SELECT 
+    MAX(salary * months) AS max_earnings,
+    COUNT(*) AS employee_count
+FROM Employee
+WHERE salary * months = (
+    SELECT MAX(salary * months)
+    FROM Employee
+);
