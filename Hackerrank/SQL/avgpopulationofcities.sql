713f217016f2c3c9702441a450b129d829074595
Sakshi Mude <sakshimude22@gmail.com>
2026-03-29 22:46:21 +0530

    Average Population of each continent

---
+++ Hackerrank/SQL/avgpopulationofcities.sql
@@ -0,0 +1,7 @@
+-- using ms sql server
+SELECT c.Continent,
+       FLOOR(AVG(ci.Population)) AS Avg_Population
+FROM CITY ci
+INNER JOIN COUNTRY c
+ON ci.CountryCode = c.Code
+GROUP BY c.Continent;
