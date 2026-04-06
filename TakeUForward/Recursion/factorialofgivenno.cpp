0a8cef6ddac1617e977f89746335ea5872bb24bc
Sakshi Mude <sakshimude22@gmail.com>
2026-04-06 22:48:44 +0530

    Factorial of Given number

---
+++ TakeUForward/Recursion/factorialofgivenno.cpp
@@ -0,0 +1,9 @@
+// Fctorial of Given number
+class Solution{	
+	public:
+		long long int factorial(int n){
+			//your code goes here
+              if (n <= 1) return 1;
+                return n* factorial(n - 1);
+		}
+};
