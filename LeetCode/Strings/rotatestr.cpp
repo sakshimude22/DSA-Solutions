7095bca1a120b926c8fdf0ec7060bab37c9f88a2
Sakshi Mude <sakshimude22@gmail.com>
2026-05-03 18:52:14 +0530

    Rotate Strings

---
+++ LeetCode/Strings/rotatestr.cpp
@@ -0,0 +1,10 @@
+// 796. Rotate Strings
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if(s.size() != goal.size()) return false;
+
+        string temp = s + s;
+        return temp.find(goal) != string::npos;
+    }
+};
