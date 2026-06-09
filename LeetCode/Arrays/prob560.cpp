f058309c0cb9be882538e8e458f7d8be62498126
Sakshi Mude <sakshimude22@gmail.com>
2026-06-09 19:59:41 +0530

    Subarray Sum Equals K

---
+++ LeetCode/Arrays/prob560.cpp
@@ -0,0 +1,38 @@
+// 560. Subarray Sum Equals K
+// Optimal approach
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int, int> mp;
+        mp[0] = 1;
+        int sum = 0;
+        int count = 0;
+        for(int n : nums){
+            sum += n;
+
+            if(mp.count(sum - k))
+            count += mp[sum - k];
+
+            mp[sum]++;
+        }
+        return count;
+    }
+};
+
+
+// brute force approach
+// class Solution {
+// public:
+//     int subarraySum(vector<int>& nums, int k) {
+//         int count = 0;
+//         for(int i = 0; i < nums.size(); i++){
+//             int sum = 0;
+//             for(int j = i; j < nums.size(); j++){
+//                 sum += nums[j];
+//                 if(sum == k)
+//                 count++;
+//             }
+//         }
+//         return count;
+//     }
+// };
