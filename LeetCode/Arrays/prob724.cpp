4bdb94f7ba75f3e9bb53f698c68d274a15a6fb06
Sakshi Mude <sakshimude22@gmail.com>
2026-06-15 21:17:49 +0530

    Find Pivot Index

---
+++ LeetCode/Arrays/prob724.cpp
@@ -0,0 +1,17 @@
+// 724. Find Pivot Index
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+       int total_sum = accumulate(nums.begin(), nums.end(), 0);
+       int leftsum = 0;
+
+       for(int i = 0; i < nums.size(); i++){
+        int rightsum = total_sum - leftsum - nums[i];
+        if(leftsum == rightsum){
+            return i;
+        }
+        leftsum += nums[i];
+       }
+       return -1;
+    }
+};
