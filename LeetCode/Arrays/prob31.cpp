0e3ca45028be37c588968d5acefedf47bee589ce
Sakshi Mude <sakshimude22@gmail.com>
2026-07-08 21:29:23 +0530

    Next Permutation

---
+++ LeetCode/Arrays/prob31.cpp
@@ -0,0 +1,23 @@
+// 31. Next Permutation
+class Solution {
+public:
+    void nextPermutation(vector<int>& arr) {
+        int n=arr.size();
+        int index=-1;
+        for(int i=n-2;i>=0;i--){
+            if(arr[i]<arr[i+1]){
+                index=i;
+                break;
+            }
+        }
+        if(index==-1) {
+            reverse(arr.begin(),arr.end());
+            return;}
+        for(int i=n-1;i>index;i--){
+            if(arr[i]>arr[index]) {
+                swap(arr[i],arr[index]);
+                break;
+        }}
+        reverse(arr.begin()+index+1,arr.end());
+    }
+};
