35e573e182b1c80e213f794c2bdd6ad87e06e19d
Sakshi Mude <sakshimude22@gmail.com>
2026-07-16 23:15:49 +0530

    Sum of GCD of Formed Pairs

---
+++ LeetCode/Arrays/prob3867.cpp
@@ -0,0 +1,22 @@
+// 3867. Sum of GCD of Formed Pairs
+class Solution {
+public:
+    long long gcdSum(vector<int>& nums) {
+        int mx = 0;
+        int n = nums.size();
+
+        for(int i = 0; i < n; i++){
+            if(nums[i] > mx) 
+            mx = nums[i];
+            nums[i] = gcd(mx, nums[i]);
+        }
+        sort(nums.begin(), nums.end());
+        int l = 0, h = n - 1;
+        long long result = 0;
+        while(l < h){
+            result += gcd(nums[l], nums[h]);
+            l++; h--;
+        }
+        return result;
+    }
+};
