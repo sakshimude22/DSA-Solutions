aa4341a02484079990b65c873190429912227559
Sakshi Mude <sakshimude22@gmail.com>
2026-07-27 10:52:31 +0530

    Leaders in an Array

--- TakeUForward/Arrays/leader_of_arr.cpp
+++ TakeUForward/Arrays/leader_of_arr.cpp
@@ -1,22 +1,21 @@
-// Leaders in an Array
 class Solution {
 public:
     vector<int> leaders(vector<int>& nums) {
       vector<int> ans;
 
-      for(int i = 0; i < nums.size(); i++){
-        bool leader = true;
+      if(nums.empty()){
+        return ans;
+      }
+      int max = nums[nums.size() - 1];
+      ans.push_back(nums[nums.size() - 1]);
 
-        for(int j = i + 1; j < nums.size(); j++){
-            if(nums[j] >= nums[i]){
-                leader = false;
-                break;
-            }
-        }
-        if(leader){
+      for(int i = nums.size() - 2; i >= 0; i--){
+        if(nums[i] > max){
             ans.push_back(nums[i]);
+            max = nums[i];
         }
       }
+      reverse(ans.begin(), ans.end());
       return ans;
     }
 };
