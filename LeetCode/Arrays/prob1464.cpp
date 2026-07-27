7a332340c5b467bc06100342a95cf79813dccf2b
Sakshi Mude <sakshimude22@gmail.com>
2026-07-27 10:12:40 +0530

    Maximum Product of Two Elements in an Array

--- LeetCode/Arrays/prob1464.cpp
+++ LeetCode/Arrays/prob1464.cpp
@@ -13,3 +13,14 @@ public:
         return ans;
     }
 };
+
+/*
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+       sort(nums.begin(), nums.end());
+       int x = nums[nums.size() - 1];
+       int y = nums[nums.size() - 2];
+       return (x - 1) * (y - 1);
+    }
+}; */
