8a5213dbf05a678c203e028b796e98e7d15099aa
Sakshi Mude <sakshimude22@gmail.com>
2026-07-27 09:55:15 +0530

    Maximum Product of Two Elements in an Array

---
+++ LeetCode/Arrays/prob1464.cpp
@@ -0,0 +1,15 @@
+// 1464. Maximum Product of Two Elements in an Array
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int ans = 0;
+        int n = nums.size();
+
+        for(int i = 0; i < n; i++){
+            for(int j = i + 1; j < n; j++){
+                ans = max(ans, (nums[i] - 1) * (nums[j] - 1));
+            }
+        }
+        return ans;
+    }
+};
