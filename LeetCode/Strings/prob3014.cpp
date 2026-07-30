919fb9812ffad88d39b1fc7c55e85bd205ff0d3f
Sakshi Mude <sakshimude22@gmail.com>
2026-07-30 19:23:22 +0530

    Minimum Number of Pushes to Type Word I

---
+++ LeetCode/Strings/prob3014.cpp
@@ -0,0 +1,8 @@
+// 3014. Minimum Number of Pushes to Type Word I
+class Solution {
+public:
+    int minimumPushes(string word) {
+        auto q = word.size() >> 3, r = word.size() & 7;
+        return ((q << 2) + r) * (q + 1);
+    }
+};
