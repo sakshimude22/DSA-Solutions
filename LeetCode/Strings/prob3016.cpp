dd94b919fc8627de9714f89faa8dcf72768fff76
Sakshi Mude <sakshimude22@gmail.com>
2026-07-31 20:29:50 +0530

    Minimum Number of Pushes to Type Word II

---
+++ LeetCode/Strings/prob3016.cpp
@@ -0,0 +1,17 @@
+// 3016. Minimum Number of Pushes to Type Word II
+class Solution {
+public:
+    int minimumPushes(string word) {
+        vector<int> freq(26, 0);
+        for( char& c : word){
+            ++freq[c -'a'];
+        }
+        sort(freq.rbegin(), freq.rend());
+        int totalpushes = 0;
+        for(int i = 0; i < 26; i++){
+            if(freq[i] == 0) break;
+            totalpushes += (i / 8 + 1) * freq[i];
+        }
+        return totalpushes;
+    }
+};
