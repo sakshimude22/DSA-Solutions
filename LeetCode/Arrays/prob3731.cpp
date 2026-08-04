50fa75d97c1c8363bfca65b9101466688f3b670d
Sakshi Mude <sakshimude22@gmail.com>
2026-08-04 09:41:55 +0530

    Find Missing Elements

---
+++ LeetCode/Arrays/prob3731.cpp
@@ -0,0 +1,15 @@
+// 3731. Find Missing Elements
+class Solution {
+public:
+    vector<int> findMissingElements(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        vector<int> ans;
+        for(int i = 0; i < n - 1; i++){
+            for(int j = nums[i] + 1; j < nums[i + 1]; j++){
+                ans.push_back(j);
+            }
+        }
+        return ans;
+    }
+};
