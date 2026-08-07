d7b645e81f516106db27cce78a10b0fd1020891c
Sakshi Mude <sakshimude22@gmail.com>
2026-08-07 18:34:25 +0530

    Smallest Divisible Digit Product I

---
+++ LeetCode/Basicmaths/prob3345.cpp
@@ -0,0 +1,21 @@
+// 3345. Smallest Divisible Digit Product I
+class Solution {
+public:
+    int smallestNumber(int n, int t) {
+        auto check = [&](int num) -> bool{
+            int product = 1;
+            while(num){
+                product *= (num % 10);
+                num /= 10;
+                if(!product){
+                    break;
+                }
+            }
+            return !(product % t);
+        };
+        while(!check(n)){
+            n++;
+        }
+        return n;
+    }
+};
