e02878b943c232dfc4a0c1fa03702a2f501df4ba
Sakshi Mude <sakshimude22@gmail.com>
2026-08-15 12:07:44 +0530

    Updated Longest Subsequence With Non-Zero Bitwise XOR

--- LeetCode/Bit_manipulation/prob3702.cpp
+++ LeetCode/Bit_manipulation/prob3702.cpp
@@ -2,19 +2,33 @@
 class Solution {
 public:
     int longestSubsequence(vector<int>& nums) {
-        int n = nums.size();
-        int totalxor = 0;
-        bool allzero = true;
+        auto tot = 0;
+        auto nonzero = 0;
 
-        for(int x: nums){
-            totalxor ^= x;
-            if(x > 0){
-                allzero = false;
-            }        
-            }
-            if(totalxor > 0){
-                return n;
-            }
-            return allzero ? 0: n - 1;
+        for(auto& n : nums){
+            nonzero |= n > 0;
+            tot ^= n;
+        }
+        return nonzero * (nums.size() - !tot);
     }
 };
+
+// class Solution {
+// public:
+//     int longestSubsequence(vector<int>& nums) {
+//         int n = nums.size();
+//         int totalxor = 0;
+//         bool allzero = true;
+
+//         for(int x: nums){
+//             totalxor ^= x;
+//             if(x > 0){
+//                 allzero = false;
+//             }        
+//             }
+//             if(totalxor > 0){
+//                 return n;
+//             }
+//             return allzero ? 0: n - 1;
+//     }
+// };
