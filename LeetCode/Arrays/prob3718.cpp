4842ac6783311c70674817ba4f37f12501392948
Sakshi Mude <sakshimude22@gmail.com>
2026-08-25 10:04:42 +0530

    Smallest Missing Multiple of K

---
+++ LeetCode/Arrays/prob3718.cpp
@@ -0,0 +1,12 @@
+// 3718. Smallest Missing Multiple of K
+class Solution {
+public:
+    int missingMultiple(vector<int>& nums, int k) {
+        unordered_set<int> seen(nums.begin(), nums.end());
+        int ans = k;
+        while(seen.count(ans)){
+            ans += k;
+        }
+        return ans;
+    }
+};
