d31eab87c7a4b9f270cccdbe75b3bb08d984e340
Sakshi Mude <sakshimude22@gmail.com>
2026-09-09 21:11:16 +0530

    Count Commas in Range II

---
+++ LeetCode/Basicmaths/prob3871.cpp
@@ -0,0 +1,12 @@
+// 3871. Count Commas in Range II
+class Solution {
+public:
+    long long countCommas(long long n) {
+        long long p = 1000, res = 0;
+        while(p <= n){
+            res+= n - p + 1;
+            p *= 1000;
+        }
+        return res;
+    }
+};
