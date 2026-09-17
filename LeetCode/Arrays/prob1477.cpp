abea9fbf6847ee6f49e6666dbdc567266a19723f
Sakshi Mude <sakshimude22@gmail.com>
2026-09-17 19:44:24 +0530

    Find Two Non-overlapping Sub-arrays Each With Target Sum

---
+++ LeetCode/Arrays/prob1477.cpp
@@ -0,0 +1,25 @@
+// 1477. Find Two Non-overlapping Sub-arrays Each With Target Sum
+class Solution {
+public:
+    int minSumOfLengths(vector<int>& arr, int target) {
+        unordered_map<int, int> pos;
+        pos[0] = -1;
+        int n = arr.size();
+        int s = 0;
+        int ans = n + 1;
+        int minL = n;
+        for (int i = 0; i < n; i++) {
+            s += arr[i];
+            if (pos.count(s - target)) {
+                int j = pos[s - target];
+                int l = i - j;
+                ans = min(ans, l + (j == -1 ? n : arr[j]));
+                minL = min(minL, l);
+            }
+            arr[i] = minL;
+            pos[s] = i;
+        }
+
+        return ans == n + 1 ? -1 : ans;
+    }
+};
