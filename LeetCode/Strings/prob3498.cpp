468f6721acc0cfd04f734bdfb9a504afe00e20f7
Sakshi Mude <sakshimude22@gmail.com>
2026-09-20 17:20:20 +0530

    Reverse Degree of a String

---
+++ LeetCode/Strings/prob3498.cpp
@@ -0,0 +1,11 @@
+// 3498. Reverse Degree of a String
+class Solution {
+public:
+    int reverseDegree(string s) {
+        int ans = 0;
+        for(int i = 1; i <= s.size(); i++){
+            ans += (26 - (s[i - 1] - 'a')) * i;
+        }
+        return ans;
+    }
+};
